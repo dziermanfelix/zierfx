c54fa30ba2397f632cde7a38b1fb3fcd315f8e12
dziermanfelix <dziermanfelix@gmail.com>
2025-07-11 05:59:54 -0500

    updating album page UI

--- src/app/albums/[artist]/[album]/page.tsx
+++ src/app/albums/[artist]/[album]/page.tsx
@@ -44,26 +44,36 @@ export default async function AlbumPage({ params }: AlbumPageProps) {
       <div className='p-2'>
         <LibraryLink />
       </div>
-      <div className='p-4 max-w-7xl mx-auto rounded'>
-        <h1 className='text-3xl font-bold'>
-          {albumRecord.name} ({formatDate(albumRecord.releaseDate)})
-        </h1>
-        <h2 className='text-xl text-gray-600'>by {artistRecord.name}</h2>
 
-        <div>
-          {albumRecord.artworkUrl && (
-            <img
-              src={albumRecord.artworkUrl}
-              alt={`${albumRecord.name} artwork`}
-              className='w-full max-w-sm rounded shadow-md'
-            />
-          )}
+      <div className='p-4 flex flex-col max-w-7xl mx-auto rounded'>
+        <div className='flex flex-row'>
+          <div className='p-2 mb-3'>
+            {albumRecord.artworkUrl && (
+              <img
+                src={albumRecord.artworkUrl}
+                alt={`${albumRecord.name} artwork`}
+                className='w-full max-w-sm rounded shadow-md'
+              />
+            )}
+          </div>
+          <div className='flex flex-col ml-4 p-2 justify-center'>
+            <h1 className='text-2xl font-bold'>{albumRecord.name}</h1>
+            <h2 className='text-xl text-gray-600'>{artistRecord.name}</h2>
+            <h2 className='text-xl text-gray-600'>{formatDate(albumRecord.releaseDate)}</h2>
+          </div>
         </div>
 
-        <ul className='list-none mt-4 ml-4'>
+        <ul className='list-none mt-4 ml-4 rounded p-2'>
           {albumRecord.tracks.map((track) => (
-            <li key={track.id}>
-              {track.number}. {track.name}
+            <li key={track.id} className='flex flex-row border rounded p-3 mb-2 justify-between'>
+              <div className='flex flex-row space-x-2'>
+                <div>{track.number}</div>
+                <div>{track.name}</div>
+              </div>
+              <div className='flex flex-row space-x-2'>
+                <button className='std-link rounded'>PLAY</button>
+                <div>TIME</div>
+              </div>
             </li>
           ))}
         </ul>
