1bfa56f2330996f5ce31555d93716b915069608e
dziermanfelix <dziermanfelix@gmail.com>
2025-07-13 14:21:29 -0500

    adding ability to close audio player

--- src/components/GlobalAudioPlayer.tsx
+++ src/components/GlobalAudioPlayer.tsx
@@ -10,13 +10,23 @@ export default function GlobalAudioPlayer() {
 
   return (
     <div className='fixed bottom-0 left-0 right-0 z-50 bg-white border-t shadow-md'>
-      <AudioPlayer
-        src={track.src}
-        trackName={track.name}
-        onEnded={playNext}
-        onNext={playNext}
-        onPrevious={playPrevious}
-      />
+      <div className='relative'>
+        <button
+          onClick={clearTrack}
+          className='absolute top-2 right-2 text-gray-500 hover:text-gray-800 text-sm'
+          aria-label='Close player'
+        >
+          X
+        </button>
+
+        <AudioPlayer
+          src={track.src}
+          trackName={track.name}
+          onEnded={playNext}
+          onNext={playNext}
+          onPrevious={playPrevious}
+        />
+      </div>
     </div>
   );
 }
