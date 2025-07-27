a50e92a5d8959395b2c552ea5e93b5b644064618
dziermanfelix <dziermanfelix@gmail.com>
2025-07-27 12:20:17 -0500

    making prisma client true singleton

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -4,10 +4,14 @@ const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
 };
 
-export const db =
+const prisma =
   globalForPrisma.prisma ??
   new PrismaClient({
     log: ['query', 'error'],
   });
 
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = db;
+if (process.env.NODE_ENV !== 'production') {
+  globalForPrisma.prisma = prisma;
+}
+
+export const db = prisma;
